3de9fac220bb52f14de29fa5a1e2fb590f35611b
kostya.yemelianov <kostya.yemelianov@ab-soft.net>
2017-07-04 17:10:23 +0300

    add navigation for auth

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { LinkContainer } from 'react-router-bootstrap';
 import Navbar from 'react-bootstrap/lib/Navbar';
 import Nav from 'react-bootstrap/lib/Nav';
 import NavItem from 'react-bootstrap/lib/NavItem';
@@ -9,15 +11,27 @@ class Header extends React.Component{
             <Navbar>
                 <Navbar.Header>
                     <Navbar.Brand>
-                        <a href="#">Site header</a>
+                        <LinkContainer to="/"><a>Application</a></LinkContainer>
                     </Navbar.Brand>
                 </Navbar.Header>
                 <Nav pullRight>
-                    <NavItem  eventKey={1} href="#">Sign in</NavItem>
+                    { this.props.authenticated
+                        ? <LinkContainer to="/signout"><NavItem  eventKey={1}>Sign out</NavItem></LinkContainer>
+                        : [
+                            <LinkContainer to="/signin" key={1}><NavItem  eventKey={2}>Sign in</NavItem></LinkContainer>,
+                            <LinkContainer to="/signup" key={2}><NavItem  eventKey={3}>Sign up</NavItem></LinkContainer>
+                            ]
+                    }
                 </Nav>
             </Navbar>
         );
     }
 }
 
-export default Header;
+//export default Header;
+
+export default connect(
+    state => ({
+        authenticated: state.auth.authenticated
+    })
+ )(Header);
